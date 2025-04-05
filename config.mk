19217d1467f816c513c8d7971eeda54464183099
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-04-05 01:45:30 -0300

    rewrite toolchain to compile the kernel with pvh note

--- config.mk
+++ config.mk
@@ -7,31 +7,32 @@ CONFIG_DIR ?= ./Config
 BUILD_DIR ?= ./Build
 OBJ_DIR ?= $(BUILD_DIR)/Obj
 
-INCLUDE_FLAGS := $(shell find $(INCLUDE_DIR) -type d | sed 's/^/-I/g')
+# Include all subdirectories recursively
+INCLUDE_FLAGS :=  $(shell find $(INCLUDE_DIR) -type d | sed 's/^/-I/')
 
-# Linker (LLVM LLD)
-LD = ld.lld
-
-LDFLAGS = -nostdlib \
-					 -T $(CONFIG_DIR)/Linker.ld
-
-# Compiler (Clang)
+# Compiler - Clang targeting ffreestanding 64-bit
 CC = clang
-CFLAGS = -ffreestanding \
-					 -nostdlib \
-					 -nostdinc \
-					 -m64 \
-					 -O0 \
-					 -c \
-					 $(INCLUDE_FLAGS) \
-					 -fno-stack-protector \
-					 -fno-strict-aliasing \
-					 -fno-omit-frame-pointer \
-					 -fno-optimize-sibling-calls \
 
-# Assembler (NASM)
+CFLAGS = -ffreestanding \
+				 -nostdlib -nostdinc \
+				 -m64 \
+				 -O2 -c \
+				 -fno-stack-protector \
+				 -fno-strict-aliasing \
+				 -fno-omit-frame-pointer \
+				 -fno-optimize-sibling-calls \
+				 $(INCLUDE_FLAGS)
+
+# Assembler - NASM for 64 ELF output
 AS = nasm
 ASFLAGS = -felf64 \
 					-O2
 
-
+# Linker - LLVM LLD for deterministic	flat elf linking
+LD = ld.lld
+LDFLAGS = -nostdlib \
+					-static \
+					-T $(CONFIG_DIR)/Linker.ld \
+					--no-dynamic-linker \
+					--no-undefined \
+					--gc-sections
