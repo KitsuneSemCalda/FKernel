f12a77aabef5ef5b5c9b81be2d7b144768b8747b
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-04-05 02:04:28 -0300

    Tools: create tests with the generator too

--- Tools/generate_new_file.sh
+++ Tools/generate_new_file.sh
@@ -15,13 +15,41 @@ MODULE_PATH="$1" # Ex: Boot/Efi
 
 HEADER_DIR="Include/$(dirname "$MODULE_PATH")"
 SRC_DIR="Src/$(dirname "$MODULE_PATH")"
+TEST_DIR="Test/$(dirname "$MODULE_PATH")"
+
 HEADER_BASENAME="$(basename "$MODULE_PATH").h"
 SRC_BASENAME="$(basename "$MODULE_PATH").c"
+TEST_BASENAME="$(basename "$MODULE_PATH").test.c"
 
 HEADER_FILE="${HEADER_DIR}/${HEADER_BASENAME}"
 SRC_FILE="${SRC_DIR}/${SRC_BASENAME}"
+TEST_FILE="${TEST_DIR}/${TEST_BASENAME}"
 
-mkdir -p "$HEADER_DIR" "$SRC_DIR"
+mkdir -p "$HEADER_DIR" "$SRC_DIR" "$TEST_DIR"
+
+generate_test() {
+  cat <<EOF
+/*
+ * Filename: $(basename "$1")
+ * Project: KitsuneBSD / FKernel - Test Suite
+ * Author: ${AUTHOR} <${EMAIL}>
+ * Created On: ${DATE}
+ * License: ${LICENSE}
+ */
+
+#include <assert.h>
+#include "${HEADER_FILE}"
+
+int main(void) {
+    // TODO: Implementar testes para $(basename "$MODULE_PATH")
+
+    // Exemplo:
+    // assert(func_x(1) == 2);
+
+    return 0;
+}
+EOF
+}
 
 generate_header() {
   cat <<EOF
@@ -41,7 +69,9 @@ EOF
 } >"$HEADER_FILE"
 
 generate_header "$SRC_FILE" >"$SRC_FILE"
+generate_test "$TEST_FILE" >"$TEST_FILE"
 
 echo "✔️ Arquivos gerados com sucesso:"
 echo " - $HEADER_FILE"
 echo " - $SRC_FILE"
+echo " - $TEST_FILE"
