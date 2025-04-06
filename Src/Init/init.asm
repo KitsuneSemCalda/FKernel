38d13e803f9717d8f0439467ee3412d69385d8b4
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-04-05 23:54:18 -0300

    use imalloc in boot stub

--- Src/Init/init.asm
+++ Src/Init/init.asm
@@ -10,12 +10,12 @@ _start:
   cli ; Disable Interrupts
   xor rbp, rbp ; Zeroing base pointer
   
-  mov rdi, 0x100000
-  mov rsi, 0x100000
+  mov rdi, 0x100000 ; Set the early allocator to start in 1MiB
+  mov rsi, 0x100000 ; Set the limit of allocator to 1MiB
   call imalloc_init
   
-  mov rdi, 16384
-  mov rsi, 16
+  mov rdi, 16384 ; Align the allocator in 16 KiB
+  mov rsi, 16 ; Align to initial 16 Bits
   call imalloc_aligned
 
   mov rsp, rax
